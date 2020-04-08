88ae8272f7a97298d96eef03ca179b45b960097f
JDCuillard <jdcuillard@gmail.com>
2020-04-08 13:53:13 -0600

    added getCardsByOwner function

--- cardbank.sol
+++ cardbank.sol
@@ -11,6 +11,21 @@ contract Cardbank is Ownable{
     
     card[] public cards;
     
-    mapping (uint32 => address) cardOwner;
+    mapping (uint => address) cardOwner;
+    mapping (address => uint32) ownerCardCount;
     
+    
+    function getCardsByOwner(address _owner) external view returns (uint[] memory) {
+        uint[] memory result = new uint[](ownerCardCount[_owner]);
+        uint counter = 0;
+        for (uint i = 0; i < cards.length; i++) {
+          if (cardOwner[i] == _owner) {
+            result[counter] = i;
+            counter++;
+          }
+        }
+        return result;
+    }
 }
+    
+
